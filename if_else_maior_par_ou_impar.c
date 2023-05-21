364e0f9dbbb2a9059d868e3bb3ecf8ab1707c750
Igor Dias <106784522+IgorDias1998@users.noreply.github.com>
2023-05-21 19:52:14 -0300

    Update if_else_maior_par_ou_impar.c

--- if_else_maior_par_ou_impar.c
+++ if_else_maior_par_ou_impar.c
@@ -1,1 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
 
+/*Faça um programa que leia dois números inteiro e mostre uma mensagem indicando se este número: 
+
+- é par ou ímpar 
+
+- e se é positivo ou negativo. 
+
+- o maior e o menor */
+
+main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	
+	int num_a, num_b, resto_a,resto_b;
+	
+	printf("Digite um valor: \n");
+	scanf("%d", &num_a);
+	
+	printf("Digite um valor: \n");
+	scanf("%d", &num_b);
+	
+	resto_a = num_a % 2;
+	resto_b = num_b % 2;
+	
+	if (resto_a == 0){
+		printf("O número %d é par.\n", num_a);
+	}
+	else
+		printf("O número %d é ímpar.\n", num_a);
+	
+	if (resto_b == 0){
+		printf("O número %d é par.\n", num_b);
+	}
+	else 
+		printf("O número %d é ímpar.\n", num_b);
+	
+	if (num_a > 0){
+		printf("O número %d é positivo.\n", num_a);
+	}
+	else
+		printf("O número %d é negativo.\n", num_a);
+		
+	if (num_b > 0){
+		printf("O número %d é positivo.\n", num_b);
+	}
+	else
+		printf("O número %d é negativo.\n", num_b);
+		
+	if (num_a > num_b){
+		printf("\nO maior número é o %d, e o menor é o %d", num_a,num_b);
+	}
+	else
+		printf("\nO maior número é o %d, e o menor é o %d", num_b, num_a);
+		
+}
